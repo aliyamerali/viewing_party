73b83706fa8ba082eeda5f2f4bfccfc75d2efb8c
Aliya Merali <merali4@gmail.com>
2021-07-08 18:53:11 -0600

    Refactor login and logout to create and destroy in sessions controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,20 @@
 class UsersController < ApplicationController
-  def login_user
-    user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      flash[:success] = "Welcome, #{user.email}!"
-      redirect_to dashboard_path
-    else
-      flash[:error] = "Invalid Credentials"
-      redirect_to root_path
-    end
-  end
-
-  def logout_user
-    session[:user_id] = nil
-    redirect_to root_path
-  end
+  # def login_user
+  #   user = User.find_by(email: params[:email])
+  #   if user && user.authenticate(params[:password])
+  #     session[:user_id] = user.id
+  #     flash[:success] = "Welcome, #{user.email}!"
+  #     redirect_to dashboard_path
+  #   else
+  #     flash[:error] = "Invalid Credentials"
+  #     redirect_to root_path
+  #   end
+  # end
+  #
+  # def logout_user
+  #   session[:user_id] = nil
+  #   redirect_to root_path
+  # end
 
   def new
     @user = User.new
