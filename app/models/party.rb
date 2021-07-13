726cacf19b3a77fe0eed3135d2a8008bc38de551
Aliya Merali <merali4@gmail.com>
2021-07-13 14:17:15 -0600

    Add host? instance method to party model

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,4 +1,8 @@
 class Party < ApplicationRecord
   belongs_to :user, foreign_key: :host_id, class_name: 'User'
   has_many :invitations, dependent: :destroy
+
+  def host?(user_id)
+    host_id == user_id
+  end
 end
