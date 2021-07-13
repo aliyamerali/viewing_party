43db76f74e58a40d52351109aa953aa19e9a47fa
Aliya Merali <merali4@gmail.com>
2021-07-12 19:43:41 -0600

    Refactor parse to helper method

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,26 +1,31 @@
 class MovieService
   def self.top40(page)
     response = Faraday.get "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['MOVIE_API_KEY']}&page=#{page}"
-    JSON.parse(response.body, symbolize_names: true)
+    parse(response)
   end
 
   def self.search(query, page)
     response = Faraday.get "https://api.themoviedb.org/3/search/movie?api_key=#{ENV['MOVIE_API_KEY']}&query=#{query}&page=#{page}"
-    JSON.parse(response.body, symbolize_names: true)
+    parse(response)
   end
 
   def self.details(id)
     response = Faraday.get "https://api.themoviedb.org/3/movie/#{id}?api_key=#{ENV['MOVIE_API_KEY']}"
-    JSON.parse(response.body, symbolize_names: true)
+    parse(response)
   end
 
   def self.credits(id)
     response = Faraday.get "https://api.themoviedb.org/3/movie/#{id}/credits?api_key=#{ENV['MOVIE_API_KEY']}"
-    JSON.parse(response.body, symbolize_names: true)
+    parse(response)
   end
 
   def self.reviews(id)
     response = Faraday.get "https://api.themoviedb.org/3/movie/#{id}/reviews?api_key=#{ENV['MOVIE_API_KEY']}"
+    parse(response)
+  end
+
+  def private_class_method.parse(response)
     JSON.parse(response.body, symbolize_names: true)
   end
+
 end
