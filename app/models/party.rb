f23a8138c34acac5f54c66d6eb51baeb81a94ee9
Aliya Merali <merali4@gmail.com>
2021-07-13 20:28:53 -0600

    Remove Party#host method and test (no longer needed)

--- app/models/party.rb
+++ app/models/party.rb
@@ -2,10 +2,6 @@ class Party < ApplicationRecord
   belongs_to :user, foreign_key: :host_id, class_name: 'User'
   has_many :invitations, dependent: :destroy
 
-  def host?(user_id)
-    host_id == user_id
-  end
-
   def invited_friends
     invitations.map do |invite|
       User.find(invite.guest_id)
