100f76fe472117a9e51bbf15bcef1ed1b1be3ec9
Alex Ferencz <alex.ferencz6@gmail.com>
2021-07-14 15:33:16 -0600

    Movie show page similar movie functionality and tests

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,6 +5,7 @@ class MoviesController < ApplicationController
     @movie = MovieFacade.details(params[:id])
     @cast = MovieFacade.first_ten_cast(params[:id])
     @reviews = MovieFacade.reviews(params[:id])
+    @similar_movies = MovieFacade.similar(params[:id])
   end
 
   def new_search; end
--- spec/poros/similar_movie_spec.rb
+++ spec/poros/similar_movie_spec.rb
@@ -2,12 +2,15 @@ require 'rails_helper'
 
 RSpec.describe 'Similar Movie PORO' do
   it 'has attributes of cast member' do
-    attributes = {author: "Reviewer Name", content: "Movie Review"}
-    review = Review.new(attributes)
-
-    expect(review.author).to eq(attributes[:author])
-    expect(review.author).to be_a String
-    expect(review.content).to eq(attributes[:content])
-    expect(review.content).to be_a String
+    attributes = {id: 1, title: "Test Movie", overview: "It's a real winner", vote_average: 8.7}
+    movie = SimilarMovie.new(attributes)
+    expect(movie.id).to eq(attributes[:id])
+    expect(movie.id).to be_a(Integer)
+    expect(movie.title).to eq(attributes[:title])
+    expect(movie.title).to be_a String
+    expect(movie.overview).to eq(attributes[:overview])
+    expect(movie.overview).to be_a(String)
+    expect(movie.vote_average).to eq(attributes[:vote_average])
+    expect(movie.vote_average).to be_a(Float)
   end
 end
